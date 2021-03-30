2b72530584f1670db3a3bac90cccdaf67cf1314e
alexaapo <alexaaposto@gmail.com>
2021-03-30 20:39:46 +0300

    next

---
+++ front-end/scru-maniac/src/components/AppNavigation.vue
@@ -0,0 +1,51 @@
+<template>
+    <span>
+        <v-toolbar app color="#7dc2ad" dark>
+            <router-link to="/">
+                <v-toolbar-title class= "purple--text" data-cy="titleBtn">{{
+                    appTitle
+                }}</v-toolbar-title>
+            </router-link>
+            <template v-for="(item,index) in items">
+                <template v-if="index <= 1">
+                    <v-btn :key="index" :to="item.url" plain> {{ item.title }} </v-btn>
+                </template>
+                <template v-else>
+                    <v-btn :key="index" :to="item.url" fixed right color=#FF914D> {{ item.title }} </v-btn>
+                </template>
+            </template>
+        </v-toolbar>
+    </span> 
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'ScruManiac',
+            drawer: false,
+            outlined: true,
+            plain: true,
+            fixed: true, 
+            right:true,
+            
+            items: [
+                { title: 'Πώς δουλέυει', url: '/how_it_works' },
+                { title: 'Τιμές', url: '/prices' },
+                { title: 'Σύνδεση', url: '/signin'}
+            ]
+        };
+    }
+};
+</script>
+
+<style>
+a {
+    color: white;
+    text-decoration: none;
+}
+.v-btn {
+  text-transform:none !important;
+}
+</style>
