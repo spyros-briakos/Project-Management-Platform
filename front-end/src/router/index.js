2d975af2f66a3b43c929c93aa8de10e80ce8804e
alexaapo <alexaaposto@gmail.com>
2021-05-22 14:44:10 +0300

    scrum edit

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -7,7 +7,7 @@ import Roadmap from "../components/Project/Roadmap.vue";
 import Comments from "../components/Project/Comments.vue";
 import History from "../components/Project/History.vue";
 import Kanban from "../components/Project/Kanban.vue";
-import Dashboard from "../components/Project/Scrumboard/Dashboard.vue";
+// import Dashboard from "../components/Project/Scrumboard/Dashboard.vue";
 import TaskBoard from "../components/Project/Scrumboard/TaskBoard.vue";
 import Chart from "../components/Project/Chart.vue";
 import SignIn from "../components/Sign/SignIn.vue";
@@ -19,11 +19,11 @@ import "../assets/scss/vue-sidebar-menu.scss";
 // import "custom-var.scss"
 import HowItWorks from "../components/HowItWorks/howItWorks.vue";
 import ProfOpts from "../components/profile/prof_opts.vue";
-    import profSettings from "../components/profile/prof_settings.vue";
-    import profProjects from "../components/profile/prof_projects.vue";
-    import profCoWorkers from "../components/profile/prof_coworkers.vue";
-    import profLogout from "../components/profile/prof_logout.vue";
-    import profUpgrade from "../components/profile/prof_upgrade.vue"
+import profSettings from "../components/profile/prof_settings.vue";
+import profProjects from "../components/profile/prof_projects.vue";
+import profCoWorkers from "../components/profile/prof_coworkers.vue";
+import profLogout from "../components/profile/prof_logout.vue";
+import profUpgrade from "../components/profile/prof_upgrade.vue"
 
     
 Vue.use(VueRouter);
@@ -51,11 +51,11 @@ const routes = [
     name: "Projects",
     component: Projects,
     children: [
-      {
-        path: "dashboard",
-        name: "dashboard",
-        component: Dashboard,
-      },
+      // {
+      //   path: "dashboard",
+      //   name: "dashboard",
+      //   component: Dashboard,
+      // },
       {
         path: "boards/:id",
         name: "task-board",
