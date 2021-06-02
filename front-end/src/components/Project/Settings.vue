49c0b45dccee6d60e8176b3d1417bf89481c6dbb
MVour <m.vourtzoumis@gmail.com>
2021-06-02 22:07:30 +0300

    prj > settings > connected form

--- front-end/src/components/Project/Settings.vue
+++ front-end/src/components/Project/Settings.vue
@@ -13,11 +13,11 @@
                     <v-icon left> mdi-account </v-icon>
                     {{item.tab}}
                 </v-tab>
-
-                <v-tab-item style="padding-bottom:10px;" v-for="item in menu" :key="item.tab + '_info'">
-                    <!-- <v-form id="prj_general_form" class="wrap_form"> -->
+                    <v-tab-item style="padding-bottom:10px;" v-for="item in menu" :key="item.tab + '_info'">
+                    
                         <v-card flat  v-if="item.form=='general'">
-                            <v-text-field style="margin-bottom:3vh;" @change="updateForm(elem.key, elem.val)" :id="item.form" v-for="elem in item.info" :key="elem.key"
+                            <v-card-subtitle style="display:flex;">Γενικές Ρυθμίσεις</v-card-subtitle>
+                            <v-text-field style="width:80%;margin:0 auto 3vh auto;" @change="updateForm(item, elem.key, elem.val)" v-for="elem in item.info" :id="elem.key" :key="elem.key"
                                 :label="elem.tag"
                                 hide-details="auto"
                                 v-model="elem.val"
@@ -27,7 +27,7 @@
                             </v-text-field>
                         </v-card>
 
-                        <v-card flat v-if="item.form=='team'">
+                        <v-card flat v-else-if="item.form=='team'">
                             <v-card flat style="margin-bottom:3vh;">
                                 <v-card-subtitle style="display:flex;">Τα Μέλη του Project:</v-card-subtitle>
                                 <v-chip-group style="width:80%;margin:0 auto 0 auto;padding:0 10px 0 10px;display:flex;" multiple column>
@@ -35,10 +35,15 @@
                                         {{co}}
                                     </v-chip>
                                 </v-chip-group>
+                                <v-btn style="margin-top:2vh;">
+                                    Θέλω να αποχωρήσω
+                                </v-btn>
                             </v-card>
+
                             <v-card flat max-width="100%" style="margin:0 auto 0 auto;">
                                 <v-card-subtitle style="display:flex;">Προσκάλεσε Νέα Άτομα</v-card-subtitle>
-                                <v-autocomplete style="width:80%;margin:0 auto 0 auto;" id="getFriends"
+                                <input id="friends" v-model="myform['addCo']">
+                                <v-autocomplete style="width:80%;margin:0 auto 0 auto;" @change="updateForm(item, 'CoWorkers', null)" id="getFriends"
                                     class="friends_picker"
                                     multiple
                                     chips
@@ -54,19 +59,30 @@
                             </v-card>
                         </v-card>
 
-                        <v-btn style="margin-top:5vh;" floating x-large :loading="loading_btn">
-                            Ενημέρωση
+                        <v-card flat v-else>
+                            <v-card-subtitle style="display:flex;">Διαγραφή Project</v-card-subtitle>
+                            <v-btn type="submit" x-large color="error" @click="updateForm(item, item.key, '1')">
+                                {{item.tag}}
+                            </v-btn>
+                        </v-card>
+
+                        <v-btn @click="mySubmit()" v-if="item.form!='deletePrj'"
+                            :disabled="item.disabled"
+                            style="margin-top:3vh;"
+                            floating x-large
+                            :loading="loading_btn">
+                                Ενημέρωση
                         </v-btn>
-                    <!-- </v-form> -->
-                </v-tab-item>
+                    
+                    </v-tab-item>
             </v-tabs>
-        <div>
+        <!-- <div>
             {{myform.title}}<br>
             {{myform.descr}}<br>
             {{myform.addCo}}<br>
             {{myform.removeCo}}<br>
             {{myform.deletePrj}}<br>
-        </div>
+        </div> -->
         </v-card>
         <router-view></router-view>
     </div>
@@ -88,6 +104,7 @@
                             {key:"descr", tag: "Περιγραφή", val:"ΩΩΩΩΩΩΩΩ αααααα ΕΕΕΕΕΕΕΕΕ"},
                             {key:"type", tag: "Τυπος Project", val: 0},
                         ],
+                        disabled: true,
                     },
                     {
                         tab: 'Η Ομάδα μας',
@@ -107,14 +124,15 @@
                             ],
                             toAdd:{key:"addCo", arr:[]},
                             toRemove:{key:"removeCo", arr:[]},
-                        }
+                        },
+                        disabled: true,
                     },
                     {
                         tab: 'Διαγραφή Project',
-                        form: "deletePr",
-                        info:[
-                            {tag: "Μόνιμη Διαγραφή Project", val: ""},
-                        ]
+                        form: "deletePrj",
+                        tag: "Μόνιμη Διαγραφή Project",
+                        key: "deletePrj",
+                        disabled: true,
                     },
                 ],
                 loading_btn: false,
@@ -125,8 +143,30 @@
             mpou(){
                 alert("on-click");
             },
-            updateForm(key, val){
-                this.myform[key] = val;
+            updateForm(item, key, val){
+                if(key != "CoWorkers")
+                    this.myform[key] = val;
+                item.disabled=false;
+            },
+            mySubmit(){ 
+                var form = this.createForm();
+                form.submit();
+            },
+            createForm(){
+                var form = document.createElement('form');
+                // form.setAttribute('method','post');
+                form.setAttribute('name','prjForm');
+                
+                let Vals=['title', 'descr', 'addCo', 'removeCo', 'deletePrj'];
+                for(let i of Vals){
+                    var item = document.createElement('input');
+                    item.setAttribute('value', this.myform[i]);
+                    form.appendChild(item);
+                }
+                console.log(form);
+                // console.log(document.forms['prj_form'].elements);
+                document.body.appendChild(form);
+                return form;
             }
         },
     };
