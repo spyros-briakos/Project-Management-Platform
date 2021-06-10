36324626e764ff9c67e2bf2582692ccd25e70ed5
Andreas <andreas.giannoutsos@gmail.com>
2021-06-10 11:00:54 +0300

    added many users at reset for testing

--- back-end/routes/db.js
+++ back-end/routes/db.js
@@ -41,6 +41,76 @@ router.post('/set-test-db', function(req, res) {
   res.json({ status : 'OK' })
 })
 
+
+// reset many users
+router.get('/set-test-db-many-users', function(req, res) {
+  db.db.dropDatabase()
+  // const passwordHash = bcrypt.hashSync('admin1234', 10);
+  var user;
+  user = new User({
+    username: 'admin',
+    password: 'admin1234',
+    email: 'dionysisbalaskas@gmail.com',
+    firstName: 'Dionysis',
+    lastName: 'Balaskas',
+    status: 'Active',
+  });
+  user.save();
+
+  user = new User({
+    username: 'admin2',
+    password: 'admin1234',
+    email: 'dionysisbalaskas@gmail.com',
+    firstName: 'Andreas',
+    lastName: 'Giannoutsos',
+    status: 'Active',
+  });
+  user.save();
+
+  user = new User({
+    username: 'admin3',
+    password: 'admin1234',
+    email: 'dionysisbalaskas@gmail.com',
+    firstName: 'Meropi',
+    lastName: 'Fanou',
+    status: 'Active',
+  });
+  user.save();
+
+  user = new User({
+    username: 'admin4',
+    password: 'admin1234',
+    email: 'dionysisbalaskas@gmail.com',
+    firstName: 'Spyros',
+    lastName: 'Mpriakos',
+    status: 'Active',
+  });
+  user.save();
+
+  user = new User({
+    username: 'admin5',
+    password: 'admin1234',
+    email: 'dionysisbalaskas@gmail.com',
+    firstName: 'Alexandra',
+    lastName: 'Apostolopoulou',
+    status: 'Active',
+  });
+  user.save();
+
+  user = new User({
+    username: 'admin6',
+    password: 'admin1234',
+    email: 'dionysisbalaskas@gmail.com',
+    firstName: 'Mixalhs',
+    lastName: 'Bourtzoumhs',
+    status: 'Active',
+  });
+  user.save();
+  // const project
+  res.json({ status : 'OK' })
+})
+
+
 // reset system
 router.get('/reset', function(req, res) {
   db.db.dropDatabase()
