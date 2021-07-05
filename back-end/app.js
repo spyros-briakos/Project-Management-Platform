e306367c11d874264996c911df17222616666321
Spyros Briakos <spympr.6@gmail.com>
2021-07-05 17:49:12 +0300

    nothing

--- back-end/app.js
+++ back-end/app.js
@@ -74,13 +74,8 @@ app.get("/", (req, res) => {
 });
 
 // SERVER CONFIG
-<<<<<<< HEAD
-const PORT = process.env.PORT || 5000
-// const PORT = 3000
-=======
 // const PORT = process.env.PORT || 5000
 const PORT = 3000
->>>>>>> 06f9c0840e58ed88834c5a5cf17c0fae3f785d50
 const HOST = process.env.HOST || '127.0.0.1'
 // const DB_URL = process.env.DB_CONNECTION || "mongodb://localhost/scrub";
 
