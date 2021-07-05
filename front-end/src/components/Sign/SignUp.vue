01cff429a0aad24f4fbbf91f9c40d71b87f40962
Andreas <andreas.giannoutsos@gmail.com>
2021-07-05 14:59:27 +0300

    sign up google

--- front-end/src/components/Sign/SignUp.vue
+++ front-end/src/components/Sign/SignUp.vue
@@ -132,7 +132,7 @@ export default {
   },
   
   methods: {
-    ...mapActions(["signup"]),
+    ...mapActions(["signup", "signupGoogle"]),
     goToSignIn(){
       this.$router.push({name:"SignIn"})
     },
